f0b102721530030fd83852eec25b84197095b0be
Juan Caicedo <juan@spanishdict.com>
2016-10-25 20:41:13 -0500

    solución 2

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -4,3 +4,12 @@ function buscar(x, y) {
   var grid = document.querySelector('.grid');
   return grid.children[y].children[x];
 }
+
+oboe('http://localhost:3000/datos')
+  .node('{x y color}', function(punto) {
+    var casilla = buscar(punto.x, punto.y);
+    casilla.classList.add(punto.color);
+  })
+  .fail(function(error) {
+    console.error('error', error);
+  });
