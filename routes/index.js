9e3bfcafc16d8b34875ca5c391b088f71247eb00
Juan Caicedo <juan@spanishdict.com>
2016-10-26 16:27:56 -0500

    solucion 3

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,22 @@
 var express = require('express');
 var fs = require('fs');
 var path = require('path');
+var highland = require('highland');
+var oboe = require('oboe');
+
 var router = express.Router();
+
+function streamDePuntos(stream) {
+  return highland(function(push, next) {
+    oboe(stream)
+      .node('{x y color}', function(punto) {
+        push(null, punto);
+      })
+      .done(function() {
+        push(null, highland.nil);
+      });
+  });
+}
 
 router.get('/', function(req, res) {
   res.render('index');
@@ -11,7 +26,19 @@ router.get('/datos', function(req, res) {
 
   var pathGato = path.resolve(__dirname, '../datos/gato.json');
   var datosGato = fs.createReadStream(pathGato);
-  datosGato.pipe(res);
+  var streamGato = streamDePuntos(datosGato);
+
+  var streamPuntos = streamGato.map(JSON.stringify)
+    .intersperse(',');
+
+  highland([
+      '[',
+      streamPuntos,
+      ']'
+    ])
+    .invoke('split', [''])
+    .sequence()
+    .pipe(res);
 
 });
 
